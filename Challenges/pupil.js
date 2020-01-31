6b521e734633e964877a53d2aba2b5ace4327109
drozsival <drozsival20@nscds.org>
2020-01-31 20:59:54 +0000

    emoji and race

---
+++ Challenges/pupil.js
@@ -0,0 +1,4 @@
+TweenMax.to(".pupil",2,{x:33,repeat:-1,yoyo:true,ease:Sine.easeOut});
+/*TweenMax.to(".pupil",2,{x:-50, delay:1, ease:Sine.easeOut});
+TweenMax.to(".pupil",2,{x:-50, y:-40, delay:2, ease:Sine.easeOut});
+TweenMax.to(".pupil",2,{x:10, y:30, delay:3, ease:Sine.easeOut});*/
