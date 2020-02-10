153636b4da08b6a49c10b604e9b2f5d279b4ead5
drozsival <drozsival20@nscds.org>
2020-02-10 15:27:47 +0000

    calculator swag

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -5,9 +5,27 @@ $('#sumButton').click(
          * HINT: parseInt() -  https://www.w3schools.com/jsref/jsref_parseint.asp 
          * 
          **/
-        var a = $('#sumA').val();
-        var b = $('#sumB').val();
+        var a = parseInt($('#sumA').val());
+        var b = parseInt($('#sumB').val());
         var sum = a + b;
         $('#sumTotal').text(sum);
     }
 );
+
+$('#subButton').click(
+    function(){
+        var a = parseInt($('#subA').val());
+        var b = parseInt($('#subB').val());
+        var sum = a - b;
+        $('#subTotal').text(sum);
+    }
+);
+
+/*
+    function(){
+        var a = parseInt($('#sumA').val());
+        var b = parseInt($('#sumB').val());
+        var sum = a  b;
+        $('#sumTotal').text(sum);
+    }
+);*/
