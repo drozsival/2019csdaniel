9482a0b5f378f4e6d46259ef72ba4d3b8f1ff246
drozsival <drozsival20@nscds.org>
2020-02-21 15:25:35 +0000

    dot game

--- Game/Game.js
+++ Game/Game.js
@@ -1,15 +1,5 @@
-init();
- 
-function init(){
-    for(var r=0; r<9; r++)
+
+    for(var r=0; r<4; r++)
         for(var c=0; c<4; c++){
-            var color = (r+c)%2===0?' black': ' white';
-            $('#finishLine').append("<div class='checker " + color + "'></div>");
+            $('#outer').append("<div class='dot'></div>");
         }
-    startRace();
-}
- 
-function startRace(){
-    TweenMax.to("#car1",2,{x:520,ease:Sine.easeOut});
-    TweenMax.to("#car1",.8,{rotation: -90,delay:1.2, ease:Sine.easeOut});
-}
