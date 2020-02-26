0a5ce32adf7c23715bace3c6223ccb10ab690647
drozsival <drozsival20@nscds.org>
2020-02-26 19:14:20 +0000

    game lit

--- Game/Game.js
+++ Game/Game.js
@@ -1,16 +1,21 @@
-    for(var r=0; r<5; r++){
+var numRows = 5;
+var numCols = 5;    
+    
+    for(var r=0; r<numRows*2; r++){
         if(r%2==0){
-            for(var c=0; c<5; c++){
+            for(var c=0; c<numCols; c++){
+                console.log("first for");
                 $('#outer').append("<div class='dot'></div>");
-                if(c<4){
+                if(c<numCols){
                     $('#outer').append("<div class='line'></div>");
                 }
             }
         }
         else{
-            for(var c=0; c<5; c++){
+            for(var c=0; c<numRows; c++){
+                console.log("second for")
                 $('#outer').append("<div class='vert'></div>");
-                if(c<4){
+                if(r<numRows-1){
                     $('#outer').append("<div class='box'></div>");
                 }
             }
